f5bc68665cf8449192a38551c45a0db2afb6e794
Documenter.jl <documenter@juliadocs.github.io>
2022-05-20 19:49:27 +0000

    build based on 13f08dd

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"index.html#TanhSinhQuadrature.jl","page":"TanhSinhQuadrature.jl","title":"TanhSinhQuadrature.jl","text":"","category":"section"},{"location":"index.html","page":"TanhSinhQuadrature.jl","title":"TanhSinhQuadrature.jl","text":"Multi-dimensional tanh-sinh quadrature in Julia","category":"page"}]
+}
